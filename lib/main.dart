4bedd3235e8584134021d1c82486ef58969156e4
dong881 <joe888joejoe@gmail.com>
2023-11-21 17:59:20 +0800

    add line chart with fixed data

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,58 @@
 import 'package:flutter/material.dart';
-import 'helpers/Constants.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:fl_chart/fl_chart.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // remove debug mark
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: appColorScheme),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: appTitle),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // 新增一個控制器來獲取新的數據
-  final TextEditingController _angleController = TextEditingController();
-  final TextEditingController _illuminationController = TextEditingController();
-
-  // 新增一個方法來上傳新的數據
-  void _uploadData() {
-    double angle = double.tryParse(_angleController.text) ?? 0.0;
-    int illumination = int.tryParse(_illuminationController.text) ?? 0;
-
-    // 上傳新的數據
-    FirebaseFirestore.instance.collection('TEST_123').add({
-      'angle': angle,
-      'illumination': illumination,
-      'time': Timestamp.now(),
-      // 其他字段...
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('TEST_123')
-                  .orderBy('time', descending: true)
-                  .limit(1)
-                  .snapshots(),
-              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Text('Loading...');
-                }
-                // 獲取文檔數據
-                final documents = snapshot.data!.docs;
-                if (documents.isNotEmpty) {
-                  var latestDocument =
-                  documents[0].data() as Map<String, dynamic>;
-
-                  // 提取特定字段的數據
-                  final angle = latestDocument['angle'];
-                  final illumination = latestDocument['illumination'];
-                  // 其他字段...
-
-                  // 做一些處理，例如將數據顯示在UI上
-                  String firestoreData = 'Angle: $angle deg \nIllumination: $illumination lux';
-
-                  return Text(
-                    firestoreData,
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  );
-                } else {
-                  return const Text('No data available.');
-                }
-              },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter 折線圖表'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: LineChart(
+            LineChartData(
+              gridData: FlGridData(show: false),
+              titlesData: FlTitlesData(show: false),
+              borderData: FlBorderData(
+                show: true,
+                border: Border.all(color: const Color(0xff37434d), width: 1),
+              ),
+              minX: 0,
+              maxX: 70,
+              minY: 0,
+              maxY: 6,
+              lineBarsData: [
+                LineChartBarData(
+                  spots: [
+                    FlSpot(0, 3),
+                    FlSpot(1, 1),
+                    FlSpot(2, 4),
+                    FlSpot(3, 2),
+                    FlSpot(4, 5),
+                    FlSpot(5, 1),
+                    FlSpot(6, 4),
+                    FlSpot(7, 3),
+                    FlSpot(10, 1),
+                    FlSpot(20, 4),
+                    FlSpot(30, 2),
+                    FlSpot(40, 5),
+                    FlSpot(50, 1),
+                    FlSpot(60, 4),
+                  ],
+                  isCurved: true,
+                  color: Colors.blue,
+                  dotData: FlDotData(show: false),
+                  belowBarData: BarAreaData(show: false),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            // 新增一個按鈕和文本輸入框
-            ElevatedButton(
-              onPressed: _uploadData,
-              child: Text('Upload Data'),
-            ),
-            SizedBox(height: 20),
-            Text('Enter new data:'),
-            TextField(
-              controller: _angleController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Angle'),
-            ),
-            TextField(
-              controller: _illuminationController,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: 'Illumination'),
-            ),
-          ],
+          ),
         ),
       ),
     );
